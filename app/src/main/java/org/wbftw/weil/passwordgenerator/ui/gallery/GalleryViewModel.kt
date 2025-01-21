e72ff0411e791b48d5274bdd892c0b1cc0769747
Weil Jimmer <me@weils.net>
2025-01-21 20:03:07 +0800

    init android app.

---
+++ app/src/main/java/org/wbftw/weil/passwordgenerator/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package org.wbftw.weil.passwordgenerator.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
