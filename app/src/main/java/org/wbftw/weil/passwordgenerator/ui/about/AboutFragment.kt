546232a77c76af8ba1a4a49c8beb6ad593c65fa1
Weil Jimmer <me@weils.net>
2025-01-21 21:31:32 +0800

    ui update.

--- app/src/main/java/org/wbftw/weil/passwordgenerator/ui/about/AboutFragment.kt
+++ app/src/main/java/org/wbftw/weil/passwordgenerator/ui/about/AboutFragment.kt
@@ -1,4 +1,4 @@
-package org.wbftw.weil.passwordgenerator.ui.slideshow
+package org.wbftw.weil.passwordgenerator.ui.about
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import org.wbftw.weil.passwordgenerator.databinding.FragmentSlideshowBinding
+import org.wbftw.weil.passwordgenerator.databinding.FragmentAboutBinding
 
-class SlideshowFragment : Fragment() {
+class AboutFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentAboutBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        val aboutViewModel =
+            ViewModelProvider(this).get(AboutViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textAbout
+        aboutViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
